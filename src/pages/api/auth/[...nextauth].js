68713b49c7c4f04db1d1689f907a247b82643e6a
Will Hall <hello@willhall.uk>
2021-03-30 18:21:06 +0100

    🚑 Add Fauna Scheme to ENV

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -6,7 +6,7 @@ import faunadb from 'faunadb';
 const faunaClient = new faunadb.Client({
     secret: process.env.FAUNADB_SECRET_KEY,
     domain: process.env.FAUNADB_URL,
-    scheme: 'http',
+    scheme: process.env.FAUNADB_SCHEME,
     port: 8443
 });
 
@@ -14,7 +14,8 @@ export default NextAuth({
     providers: [
         Providers.GitHub({
             clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET
+            clientSecret: process.env.GITHUB_SECRET,
+            scope: 'user:email'
         }),
         Providers.Email({
             server: {
